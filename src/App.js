1db4ef8f33567da33525a17012d7cd1ca8286cd0
I324094 <natalie.zhang01@sap.com>
2019-03-14 17:16:20 +0800

    Enable delete table data

--- src/App.js
+++ src/App.js
@@ -2,10 +2,41 @@ import React, { Component } from 'react';
 import Table from './Table';
 
 class App extends Component {
+  state = {
+    people: [{
+      'name': 'Charlie',
+      'job': 'Janitor'
+    },
+    {
+      'name': 'Mac',
+      'job': 'Bouncer'
+    },
+    {
+      'name': 'Dee',
+      'job': 'Aspring actress'
+    },
+    {
+      'name': 'Dennis',
+      'job': 'Bartender'
+    }]
+  };
+
+  removePerson = index => {
+    const { people } = this.state;
+
+    this.setState({
+      people: people.filter((person, i) => {
+        return i !== index;
+      })
+    })
+  };
+
   render() {
+    const { people } = this.state;
+
     return (
-      <div className="App">
-        <Table />
+      <div className="container">
+        <Table tableData={people} removePerson={this.removePerson} />
       </div>
     )
   }
--- src/Table.js
+++ src/Table.js
@@ -6,40 +6,35 @@ const TableHeader = () => {
       <tr>
         <th>Name</th>
         <th>Job</th>
+        <th>Remove</th>
       </tr>
     </thead>
   );
 }
 
-const TableBody = () => {
-  return (
-    <tbody>
-      <tr>
-        <td>Charlie</td>
-        <td>Janitor</td>
+const TableBody = (props) => {
+  const rows = props.tableData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td><button type="button" className="btn btn-primary" onClick={() => props.removePerson(index)}>Delete</button></td>
       </tr>
-      <tr>
-        <td>Mac</td>
-        <td>Bouncer</td>
-      </tr>
-      <tr>
-        <td>Dee</td>
-        <td>Aspiring actress</td>
-      </tr>
-      <tr>
-        <td>Dennis</td>
-        <td>Bartender</td>
-      </tr>
-    </tbody>
-  );
+    );
+  });
+
+  return <tbody>{rows}</tbody>;
 }
 
 class Table extends Component {
   render() {
+    // destructuring assignment
+    const { tableData, removePerson } = this.props;
+
     return (
       <table className="table">
         <TableHeader />
-        <TableBody />
+        <TableBody tableData={tableData} removePerson={removePerson} />
       </table>
     )
   }
